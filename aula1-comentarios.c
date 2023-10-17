c45729c703f5f063c7327575096b3bccab39e119
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-17 08:19:56 -0300

    Create aula1-comentarios.c
    Comentar os códigos que você escreve é importante por diversos motivos e o principal deles é para que o seu eu do futuro entenda o que seu eu do passado fez, incluindo as besteiras rsrs.

---
+++ aula1-comentarios.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+  // aqui temos um comentário de apenas uma linha
+  
+  /*
+          Aqui
+          temos um bloco de comentário
+          com várias linhas.
+  */
+  
+  return 0;
+}
