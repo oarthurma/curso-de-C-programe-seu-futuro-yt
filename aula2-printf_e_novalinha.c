f2bc22295f8eef64128bb3c99b03b2873283396f
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-17 08:28:27 -0300

    Create aula2-printf_e_caracter-de-escape.c
    Fazendo uso apenas da função printf() e do caractere de escape \n é possível imprimir menus de opções para que o usuário escolha o que ele deseja fazer.

---
+++ aula2-printf_e_novalinha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+// caractere de escape para saltar uma linha -> \n
+
+int main() {
+  
+  // printf é uma função de saída
+  printf("\n--------------------------------------------\n\n);
+  printf("\n1 - Logar  2 - Cadastrar  3 - Imprimir\n");
+  printf("\n--------------------------------------------\n);
+
+  printf("\nValor retornado: %d", printf("Oi"));
+ 
+  
+  return 0;
+}
