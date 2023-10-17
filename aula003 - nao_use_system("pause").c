2a02dd123cf85d689a551134995022cda8e17156
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-17 08:33:34 -0300

    Create aula003 - nao_use_system("pause").c
    Nessa videoaula você irá aprender porque você não deve utilizar a função system ("pause") com a linguagem de programação C .
    
    Quando fazemos uso de uma IDE como o Code Blocks, ao compilar e executar nosso programa, o terminal permanece aberto mesmo após a finalização do nosso programa. Isso permite avaliar a saída gerada pelo nosso código.
    
    Dependendo do editor que esteja sendo utilizado e como está sendo feita a compilação, pode ser que o terminal fique aberto apenas durante o processo de execução do programa, fechando assim que o programa finaliza. Isso gera aquela sensação de que o terminal apenas "piscou" na tela, abrindo e fechando em seguida.
    
    Nesse momento muitos utilizam a função system ("pause") do Sistema Operacional Windows para pausar o terminal, permitindo assim verificar a saída gerada pelo programa.
    
    O maior problema é que, como mencionei, esta é uma função do sistema e não da linguagem C, ou seja, ela não existe em outros sistemas operacionais. Na verdade ela nem é necessária, uma vez que o mesmo efeito pode ser obtido de maneiro muito fácil fazendo uso das funções da biblioteca padrão da linguagem C, como apresentado na aula de hoje.
    
    E

---
+++ aula003 - nao_use_system("pause").c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+// aula 3 system("pause") - evite seu uso, pois só funciona no SO Windowns
+
+int main() {
+  
+  // printf é uma função de saída
+  printf("\n--------------------------------------------\n\n);
+  printf("\n1 - Logar  2 - Cadastrar  3 - Imprimir\n");
+  printf("\n--------------------------------------------\n);
+
+  printf("\nValor retornado: %d", printf("Oi"));
+
+
+
+
+
+
+
+
+  printf("\nPressione qualquer tecla para finalizar.");
+  getchar();
+  
+  return 0;
+}
