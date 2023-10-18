88764388f2b72370cb13f39ecc4cb5d63d5ef8ee
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-18 06:46:03 -0300

    Create aula010-lendo-varios-valores.c

---
+++ aula010-lendo-varios-valores.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+            Aula 10
+        Lendo vários valores
+*/
+
+int main() {
+
+    int num1, num2, num3;
+
+    printf("Digite tres valores inteiros: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+
+    printf("Valores lidos: %d, %d e %d\n", num1, num2, num3);
+
+    return 0;
+}
