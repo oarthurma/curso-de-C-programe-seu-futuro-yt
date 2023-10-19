91dd060369c3016f9bf92f1ee4918673b82fd670
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-19 08:04:43 -0300

    Create aula017-operador-short-para-o-tipo-int.c

---
+++ aula017-operador-short-para-o-tipo-int.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    /*
+                    Aula 17
+            operador short para o tipo int
+            intervalo: -32.768 até 32.767
+            %hi ou %d
+    */
+
+int main() {
+
+    int y = 0;
+    short int x = 32767;
+
+    printf("Valor de x: %d\n", x);
+    x++;
+    printf("Valor de x: %hi\n", x);
+
+    // 4 * 8 = 32 bits
+
+    return 0;
+}
