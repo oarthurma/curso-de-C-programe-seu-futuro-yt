1fae3a5e3e37097acb6408bbfca2b3e0ea0746e9
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-19 07:37:22 -0300

    Create aula015-unidade-de-medidas.c

---
+++ aula015-unidade-de-medidas.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+                Aula 15
+        Unidade de Medidas na Computação
+        
+        Bit -  A palavra bit vem do inglês e é uma abreviação de binary digit.
+               É a menor unidade de armazenamento, suportando um único dígito binário zero e um. 
+        
+        8 bits formam 1 byte. ex.: 01001001 (73 no sistema decimal)
+        
+        8 bits = 1 byte
+        1024 bytes = 1 Kilobyte (kb)
+        1024 Kilobytes = 1 Megabyte (mb)
+        1024 Megabytes = 1 Gigabyte (gb)
+        1024 Gigabytes = 1 Terabyte (tb)
+        1024 Terabytes = 1 Pentabyte (pb)
+        1024 Pentabytes = 1 Exabyte (eb)
+        1024 Exabytes = 1 Zettabyte (zb)
+        1024 Zettabytes = 1 Yottabyte (yb)
+        
+*/
+
+int main() {
+
+    
+
+    return 0;
+}
