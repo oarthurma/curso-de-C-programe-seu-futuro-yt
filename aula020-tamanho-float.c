d89993e4a93c702c3bb560729ff371b0cfcc7c30
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-20 07:34:54 -0300

    Create aula020-tamanho-float.c

---
+++ aula020-tamanho-float.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+                    Aula 20
+         tamanho de um float na memória
+         %f
+*/
+
+int main() {
+
+    float x = 3.1415;
+
+    printf("Um float precisa de %d bytes de memoria.\n", sizeof x);
+
+    return 0;
+}
