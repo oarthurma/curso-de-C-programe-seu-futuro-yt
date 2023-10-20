5bd49db48844adce251e159ab4492ca83d5c9395
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-20 07:40:59 -0300

    Create aula021-tipo-double.c

---
+++ aula021-tipo-double.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+                    Aula 21
+         tipo double
+         %lf
+*/
+
+int main() {
+
+    float x = 3.1415;
+
+    double y = 314155896587458962548;
+
+    printf("Um double precisa de %d bytes de memoria.\n", sizeof y);
+
+    return 0;
+}
