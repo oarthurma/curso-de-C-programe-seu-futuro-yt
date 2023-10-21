ebf4af1a09e24dd8eb59c101990d1d3ff352f532
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-21 07:19:24 -0300

    Create aula026-resto-da-divisao.c

---
+++ aula026-resto-da-divisao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+                    Aula 26
+              operador de resto da divisão
+*/
+
+int main() {
+
+    int a = 7, b = 21;
+
+    printf("Resto da divisao inteira: %d\n", b % 2);
+
+    return 0;
+}
