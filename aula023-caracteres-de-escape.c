cc4166e6b04091aa9e20e7242d5dba04c8783885
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-21 06:56:45 -0300

    Create aula023-caracteres-de-escape.c

---
+++ aula023-caracteres-de-escape.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+                    Aula 23
+            caracteres de escape
+            Quebra de linha -> \n 
+            Tabulação       -> \t
+            Aspa dupla      -> \"
+            Aspa simples    -> \'
+            Contrabarra     -> \\
+*/
+
+int main() {
+
+    printf("\n\n\tIsto e uma mensagem \'a\' \\ ser impressa na tela.\n\n");
+
+    return 0;
+}
