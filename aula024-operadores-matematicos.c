76419915ef379db41ba46315bfbd4d48ff94c2d8
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-21 07:03:34 -0300

    Create aula024-operadores-matematicos.c

---
+++ aula024-operadores-matematicos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+                    Aula 24
+            operadores matemáticos para:
+            Soma            +
+            Subtração       -
+            Multiplicação   *
+            Divisão         /
+*/
+
+int main() {
+
+    printf("\nSoma: %d\n", 10 + 20);
+    printf("\nSubtracao: %d\n", 10 - 20);
+    printf("\nMultiplicacao: %d\n", 10 * 20);
+    printf("\nDivisao: %f\n", 10.0 / 20);
+
+    return 0;
+}
