a617fd5c9723e446d63859a35aec202160a91fb9
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-21 08:55:20 -0300

    Create ex01_listaI.c

---
+++ exercicios/ex01_listaI.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+            Exercício 01 - Aula 11
+    Ler sexo idade, peso e altura
+*/
+
+
+int main() {
+
+    char sexo;
+    int idade;
+    float peso, altura;
+
+    printf("Digite sexo (f, F, m ou M), idade, peso e altura:\n");
+    scanf("%c%d%f%f", &sexo, &idade, &peso, &altura);
+
+    printf("Sexo: %c\nIdade: %d\nPeso: %.1f\nAltura: %.2f\n", sexo, idade, peso, altura);
+
+    return 0;
+}
