183f5e1cc20ca705d361940bf747f69ddd7c2eaf
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-28 11:47:42 -0300

    Create aula038-operadores-relacionais.c

---
+++ aula038-operadores-relacionais.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+        Aula 38: Operadores relacionais
+            menor que (a < b)       => 1
+            maior que (a > b)       => 0
+            menor ou igual (a <= b) => 1
+            maior ou igual (a >= b) => 0
+            igual (a == b)          => 0    / 1
+            diferente (a != b)      => 1    / 0
+
+*/
+int main() {
+
+    int a = 10, b = 20;
+
+    printf("\n\tResultado: %d\n\n", a != b);
+    return 0;
+}
