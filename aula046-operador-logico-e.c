c5cbb73dabe446f3708cf6e7d92728b6b5aa6399
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-10-30 07:47:15 -0300

    Create aula046-operador-logico-e.c

---
+++ aula046-operador-logico-e.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+            Aula 46: Operador lógico e (&)
+            TABELA VERDADE  a   b   a & b
+                            V   V     V
+                            v   f     f
+                            f   v     f
+                            f   f     f
+*/
+int main() {
+    int idade;
+    char sexo;
+
+    printf("Digite seu sexo f ou m e sua idade: ");
+    scanf("%c%d", &sexo, &idade);
+
+    if(sexo == 'm' && idade == 18) 
+        printf("Alistamento obrigatorio!\n");
+    else
+        printf("Dispensado!\n");
+    return 0;
+}
