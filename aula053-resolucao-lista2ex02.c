e9d00d32d87409301813a741c47b8f723495657a
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-05 08:21:43 -0300

    Create aula053-resolucao-lista2ex02.c

---
+++ aula053-resolucao-lista2ex02.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+                Aula 52: Exercício 02
+            Escreva um programa em C que lê 5 números inteiros, um por vez.
+            Conte quantos destes valores são negativos e quantos são positivos.
+            Ao final, imprima na tela a quantidade de números negativos e positivos.
+*/
+int main() {
+    int valor, positivos = 0, negativos = 0;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &valor);
+
+    if(valor < 0)
+        negativos++;// negativos += 1; negativos = negativos + 1;
+    else
+        positivos++;
+
+    printf("Digite o segundo valor: ");
+    scanf("%d", &valor);
+
+    if(valor < 0)
+        negativos++;
+    else
+        positivos++;
+
+    printf("Digite o terceiro valor: ");
+    scanf("%d", &valor);
+
+    if(valor < 0)
+        negativos++;
+    else
+        positivos++;
+
+    printf("Digite o quarto valor: ");
+    scanf("%d", &valor);
+
+    if(valor < 0)
+        negativos++;
+    else
+        positivos++;
+
+    printf("Digite o quinto valor: ");
+    scanf("%d", &valor);
+
+        if(valor < 0)
+            negativos++;
+        else
+            positivos++;
+
+    printf("\nPositivos: %d\nNegativos: %d\n", positivos, negativos);
+}
