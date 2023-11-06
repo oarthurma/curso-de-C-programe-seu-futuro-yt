868e54163236de7d330c814c701fec03061eafe1
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-06 06:31:46 -0300

    Update aula053-resolucao-lista2ex02.c

--- aula053-resolucao-lista2ex02.c
+++ aula053-resolucao-lista2ex02.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
-                Aula 52: Exercício 02
+                Aula 53: Exercício 02
             Escreva um programa em C que lê 5 números inteiros, um por vez.
             Conte quantos destes valores são negativos e quantos são positivos.
             Ao final, imprima na tela a quantidade de números negativos e positivos.
