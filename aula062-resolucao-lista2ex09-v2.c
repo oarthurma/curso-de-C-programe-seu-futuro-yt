4b0051034d7a151e88184858badb4ccd6b5e2192
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-08 18:19:18 -0300

    Create aula062-resolucao-lista2ex09-v2.c

---
+++ aula062-resolucao-lista2ex09-v2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+                Aula 62: Exercício 09 versão 2k
+        Um usuário deseja um programa onde possa escolher que tipo de média deseja
+        calcular a partir de três notas. Faça um programa que leia as notas e o tipo
+        da média escolhida pelo usuário e calcule e apresente a média:
+        Opções:
+        • 'a' - Aritmética.
+        • 'p' - Ponderada (pesos: 3, 3, 4).
+
+*/
+
+int main(){
+     float nota1, nota2, nota3;
+     char escolha;
+
+     printf("Qual a media deseja?\na - aritmetica\np - ponderada\n");
+     scanf("%c", &escolha);
+
+     if(escolha == 'a' || escolha == 'p') {
+     printf("Digite as tres notas: ");
+     scanf("%f%f%f", &nota1, &nota2, &nota3);
+     }
+
+     if(escolha == 'a')
+        printf("Media aritmetica: %.2f\n", (nota1 + nota2 + nota3) / 3);
+     else
+        if(escolha == 'p')
+            printf("Media ponderada: %.2f\n", (nota1 * 3 + nota2 * 3 + nota3 * 4) / 10);
+        else
+            printf("\nOpcao invalida!!!\n");
+}
