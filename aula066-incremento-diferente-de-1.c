088ff5589e9b70a3a5f8f5315aae8da027439b6a
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-09 08:25:41 -0300

    Create aula066-incremento-diferente-de-1.c

---
+++ aula066-incremento-diferente-de-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//                Aula 66: Incremento diferente de 1
+
+int main(){
+    int i;
+
+    for(i = 1; i <= 100; i+=2)// i = i + 2
+            printf("%d ", i);
+}
