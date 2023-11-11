ba893c88317ebe88517b54224c54de7785dacb56
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-11 17:48:27 -0300

    Create lista3_ex09.c

---
+++ exercicios/lista3_ex09.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+9) Faça um programa que calcule o valor de A, dado por:
+A = 1 + 2 + 3 + 4 + … + n, onde n é um número inteiro, maior que zero informado pelo usuário.
+*/
+int main(){
+
+    int n, i, A;
+
+    do{
+        printf("Digite um numero inteiro: ");
+        scanf("%d", &n);
+    }while(n <= 0);
+
+    for(i = 1; i <= n; i++)
+        A = A + i;
+
+    printf("Valor de A = %d\n", A);
+
+
+
+
+
+
+}
