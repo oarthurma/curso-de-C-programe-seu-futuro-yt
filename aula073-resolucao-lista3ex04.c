e7eea15b39d27db58d1d5bc451d90855a8cd4e2b
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-12 07:00:39 -0300

    Update aula073-resolucao-lista3ex04.c

--- aula073-resolucao-lista3ex04.c
+++ aula073-resolucao-lista3ex04.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*                Aula 73: Exercício 04
-        3) Faça um programa que some os números ímpares entre 1 e 1000 e imprima a resposta.
+        4) Faça um programa que some os números ímpares entre 1 e 1000 e imprima a resposta.
         Restrição:
         -> O bloco de repetição deve excutar no máximo 500 vezes.
 */
