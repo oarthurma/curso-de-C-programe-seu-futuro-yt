a2f2c4724ba0e57cd291623b6130c61d9abb20e3
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-11-12 07:29:51 -0300

    Update aula075-resolucao-lista3ex06.c

--- aula075-resolucao-lista3ex06.c
+++ aula075-resolucao-lista3ex06.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*                Aula 75: Exercício 06
-        4) Faça um programa que imprima na tela todos os múltiplos de 7 entre 1 e 9999;
+        6) Faça um programa que imprima na tela todos os múltiplos de 7 entre 1 e 9999;
 */
 int main(){
     int i;
