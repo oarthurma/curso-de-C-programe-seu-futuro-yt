9a373cc105b34772f6cb3a937dd269dbd71584ba
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-07 08:04:30 -0300

    Create aula096-resolucao-lista3ex25.c

---
+++ aula096-resolucao-lista3ex25.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*                Aula 95: Exercício 24
+        25) Escreva um programa que leia dois valores X e Y. A seguir, mostre uma sequência
+        de 1 até Y, passando para a próxima linha a cada X números.
+        Exemplo de entrada:
+        3 99
+        Exemplo de saída:
+
+        1 2 3
+        4 5 6
+        7 8 9
+        10 11 12
+        ...
+        97 98 99
+*/
+
+int main() {
+
+    int x, y, i;
+
+    printf("Digite X e Y: ");
+    scanf("%d%d", &x, &y);
+
+    for(i = 1; i <= y; i++){
+        printf("%3d ", i);
+        if(i % x == 0)
+            printf("\n");
+    }
+
+
+    return 0;
+}
