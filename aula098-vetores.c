e6ea87144ba8ff53b07301ad9b5aa5ce7777bc14
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-08 07:01:07 -0300

    Create aula098-vetores.c

---
+++ aula098-vetores.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*                Aula 98 - Estrutura de dados homogênea do tipo vetor (array unidimensional)
+
+        Introdução à estrutura de dados homogênea do tipo vetor (array unidimensional)
+
+Até o momento, para cada valor a ser armazenado em memória era necessário criar uma variável.
+Para ler a idade do usuário é necessário uma variável do tipo inteiro, para ler duas notas
+de um estudante são necessárias duas variáveis do tipo real.
+Contudo, e se for necessário ler a idade de 50 pessoas? Criaremos 50 variáveis?
+E se for necessário ler duas notas de 50 estudantes? Criaremos 100 variáveis?
+
+São situações desse tipo que tornam as estruturas de dados homogêneas como vetor e matriz
+tão úteis.
+
+Até o momento cada variável era capaz de armazenar um único valor.
+Agora veremos como armazenar um conjunto de valores em uma única variável.
+
+Vamos começar com os vetores (arrays unidimensionais).
+
+Um vetor é um conjunto de elementos do mesmo tipo (por isso estrutura de dado homogênea)
+armazenados de forma sequencial na memória. Assim, um vetor que armazena 10 números inteiros pode ser representado como 10 caixinhas sequenciais na memória do computador, cada uma com um valor.
+
+Como agora temos um conjunto de valores (cada vetor tem seu tamanho definido no momento da
+criação do vetor), precisamos usar um índice para acessar cada elemento do vetor, assim:
+0 - para acessar o primeiro elemento
+1 - para acessar o segundo elemento
+2 - para acessar o terceiro elemento
+e assim por diante.
+
+Eu não vou explicar agora porque inicia no índice 0 e não no índice 1.
+Isso será explicado mais a frente no curso quando formos estudar ponteiros.
+
+Por enquanto, não esqueça que o primeiro índice válido de um vetor é o índice 0.
+Assim, um vetor de tamanho 10 possui os índices de 0 a 9.
+
+*/
+
+int main() {
+
+    return 0;
+}
