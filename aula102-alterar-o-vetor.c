31ac682d714d60cd55bf97b0b85c29658e7f7973
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-09 07:27:48 -0300

    Create aula102-alterar-o-vetor.c

---
+++ aula102-alterar-o-vetor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//          Aula 102: Como alterar o conteúdo de um vetor?
+
+int main() {
+
+    int i;
+    int num2[10];
+
+    for(i = 0; i < 10; i++){
+        printf("Digite o elemento da posicao %d: ", i);
+        scanf("%d", &num2[i]);
+    }
+
+    for(i = 0; i < 10; i++)
+        num2[i] = num2[i] * 3;
+
+    printf("\n\n");
+    for(i = 0; i < 10; i++)
+        printf("%d ", num2[i]);
+    printf("\n\n");
+
+    return 0;
+}
