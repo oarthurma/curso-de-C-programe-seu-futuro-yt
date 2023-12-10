1569b445b3d44a5dbc1fbf63b67c1810daff6f5d
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-10 08:35:42 -0300

    Create aula105-o-que-sao-matrizes.c

---
+++ aula105-o-que-sao-matrizes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+        Aula 105: O que são matrizes (arrays bidimensionais)?
+
+        Assim como os vetores, uma matriz também é um conjunto de elementos do mesmo tipo
+        (por isso estrutura de dado homogênea).
+        Contudo, enquanto um vetor possui apenas uma dimensão, sendo representado
+        normalmente por uma linha ou coluna, uma matriz é um conjunto bidimensional,
+        possuindo linhas e colunas.
+
+        Assim como em um vetor, cada elemento de uma matriz pode ser acessado
+        por meio de seus índices. Enquanto o vetor, uma estrutura unidimensional,
+        exige apenas um índice, para acessar um elemento de uma matriz precisamos
+        de dois índices, um para identificar a linha e outro para identificar a coluna.
+
+        Assim, para acessar os elementos de uma matriz temos:
+        0 0 - para acessar o elemento na linha zero e coluna zero.
+        1 0 - para acessar o elemento da linha um e coluna zero.
+        0 2 - para acessar o elemento da linha zero e coluna dois.
+        e assim por diante.
+
+        Eu não vou explicar agora porque iniciar no índice 0 e não no índice 1.
+        Isso será explicado mais a frente quando formos estudar ponteiros.
+
+        Por enquanto, não esqueça que o primeiro índice válido de uma matriz,
+        assim como nos vetores, é o índice 0. Assim, uma matriz 3 x 4 (3 linhas e 4 colunas)
+        possui os índices de 0 a 2 para as linhas e de 0 a 3 para as colunas.
+
+*/
+int main() {
+
+    return 0;
+}
