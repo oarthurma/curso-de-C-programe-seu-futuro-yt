e8aa753407e5a98eaa3c1031cdbaba252730ecc4
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-10 08:58:42 -0300

    Create aula107-imprimindo-uma-matriz.c

---
+++ aula107-imprimindo-uma-matriz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+        Aula 107: Como imprimir o conteúdo de uma matriz?
+*/
+int main() {
+
+    int i, j, mat1[3][3] = {1,2,3,4,5,6,7,8,9};
+    char mat5[3][4] = {'a','b','c','d','e','f','g','h','i','j','k','l'};
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%d ", mat1[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
