3e48779df40d7d6c2f3f8fe162a455091639ce99
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-11 06:56:28 -0300

    Create aula108-lendo-matriz-teclado.c

---
+++ aula108-lendo-matriz-teclado.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+        Aula 108: Como ler uma matriz do teclado?
+*/
+int main() {
+
+    int i, j, mat1[3][3] = {1,2,3,4,5,6,7,8,9};
+    char mat5[3][4] = {'a','b','c','d','e','f','g','h','i','j','k','l'};
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("Digite o valor %d %d: ", i, j);
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            printf("%d ", mat1[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
