189e2c96209c7a391c3259e23a3a046e14b48f7a
Arthur Martins Alves <121382433+oarthurma@users.noreply.github.com>
2023-12-12 07:05:48 -0300

    Update aula112-resolucao-lista4ex02.c

--- aula112-resolucao-lista4ex02.c
+++ aula112-resolucao-lista4ex02.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /*
-2) Faça um programa que some o conteúdo de dois vetores de tamanho 25 e armazene o resultado
-em um terceiro vetor. Imprima os três vetores na tela.
+            Aula 112: Exercício 02
+        2) Faça um programa que some o conteúdo de dois vetores de tamanho 25 e armazene o
+        resultado em um terceiro vetor. Imprima os três vetores na tela.
 */
-int main(){
-    int vet1[25] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25};
-    int vet2[25] = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33, 34, 35};
-    int i, vet3[25];
+int main() {
 
-    for(i = 0; i < 25; i++)
+    int i, vet1[25], vet2[25], vet3[25];
+
+    srand(time(NULL));
+
+    for(i = 0; i < 25; i++){
+        vet1[i] = rand() % 100;
+        vet2[i] = rand() % 100;
+    }
+
+    for(i = 0; i < 25; i++){
         vet3[i] = vet1[i] + vet2[i];
+    }
 
-    printf("vet1\n");
-        for(i = 0; i < 25; i++)
-            printf("%2d ", vet1[i]);
+    printf("\nVetor 1: ");
+    for(i = 0; i < 25; i++){
+        printf("%3d ", vet1[i]);
+    }
 
-    printf("\n\nvet2\n");
-        for(i = 0; i < 25; i++)
-            printf("%d ", vet2[i]);
+    printf("\nVetor 2: ");
+    for(i = 0; i < 25; i++){
+        printf("%3d", vet2[i]);
+    }
 
-    printf("\n\nvet3\n");
-        for(i = 0; i < 25; i++)
-            printf("%d ", vet3[i]);
-        printf("\n");
+     printf("\nVetor 3: ");
+    for(i = 0; i < 25; i++){
+        printf("%3d", vet3[i]);
+    }
 
     return 0;
 }
